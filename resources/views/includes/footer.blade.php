935356100af98c997172532716fd8dbbc83b5601
Pandu Waskito <pwaskito611@gmail.com>
2021-04-06 16:20:27 +0700

    edit footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,4 @@
 <div class="mt-10"></div>
 <footer class="py-4 bg-red-600  mt-auto">
-    <p class="text-center text-white">Made by Pandu Waskito</p>
+    <p class="text-center text-white">Copyright © 2021 Merankin</p>
 </footer>
